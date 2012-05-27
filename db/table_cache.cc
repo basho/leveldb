e40220136d5554c386f2d77764cda29a4aaa8fe0
Matthew V <matthewv@basho.com>
2012-05-27 12:34:51 -0400

    Add table cache performance counters.  Use gcc atomic operations on counters.

--- db/table_cache.cc
+++ db/table_cache.cc
@@ -69,12 +69,12 @@ Status TableCache::FindTable(uint64_t file_number, uint64_t file_size,
       tf->file = file;
       tf->table = table;
       *handle = cache_->Insert(key, tf, 1, &DeleteEntry);
-      ++gPerfCounters->m_TableOpened;
+      __sync_add_and_fetch(&gPerfCounters->m_TableOpened, 1);
     }
   }
   else
   {
-      ++gPerfCounters->m_TableCached;
+      __sync_add_and_fetch(&gPerfCounters->m_TableCached, 1);
   }   // else
   return s;
 }
--- util/env_posix.cc
+++ util/env_posix.cc
@@ -26,8 +26,6 @@
 #include "util/posix_logger.h"
 #include "leveldb/perf_count.h"
 
-#include <syslog.h>
-
 #if _XOPEN_SOURCE >= 600 || _POSIX_C_SOURCE >= 200112L
 #define HAVE_FADVISE
 #endif
@@ -134,7 +132,7 @@ class PosixMmapReadableFile: public RandomAccessFile {
   PosixMmapReadableFile(const std::string& fname, void* base, size_t length, int fd)
       : filename_(fname), mmapped_region_(base), length_(length), fd_(fd)
   {
-    ++gPerfCounters->m_ROFileOpen;
+      __sync_add_and_fetch(&gPerfCounters->m_ROFileOpen, 1);
 
 #if defined(HAVE_FADVISE)
     posix_fadvise(fd_, 0, length_, POSIX_FADV_RANDOM);
@@ -253,7 +251,7 @@ class PosixMmapFile : public WritableFile {
         file_offset_(0),
         pending_sync_(false) {
     assert((page_size & (page_size - 1)) == 0);
-    ++gPerfCounters->m_RWFileOpen;
+    __sync_add_and_fetch(&gPerfCounters->m_RWFileOpen, 1);
   }
 
 
@@ -722,11 +720,11 @@ void PosixEnv::BGThread() {
     PthreadCall("unlock", pthread_mutex_unlock(&mu_));
 
   if (bgthread3_==pthread_self())
-      ++gPerfCounters->m_BGCloseUnmap;
+      __sync_add_and_fetch(&gPerfCounters->m_BGCloseUnmap, 1);
   else if (bgthread2_==pthread_self())
-      ++gPerfCounters->m_BGCompactImm;
+      __sync_add_and_fetch(&gPerfCounters->m_BGCompactImm, 1);
   else
-      ++gPerfCounters->m_BGNormal;
+      __sync_add_and_fetch(&gPerfCounters->m_BGNormal, 1);
 
     (*function)(arg);
   }
@@ -772,7 +770,7 @@ void BGFileCloser(void * arg)
 
     close(file_ptr->fd_);
     delete file_ptr;
-    ++gPerfCounters->m_ROFileClose;
+    __sync_add_and_fetch(&gPerfCounters->m_ROFileClose, 1);
 
     return;
 
@@ -797,7 +795,7 @@ void BGFileCloser2(void * arg)
     close(file_ptr->fd_);
     delete file_ptr;
 
-    ++gPerfCounters->m_RWFileClose;
+    __sync_add_and_fetch(&gPerfCounters->m_RWFileClose, 1);
 
     return;
 
@@ -817,7 +815,7 @@ void BGFileUnmapper(void * arg)
 #endif
 
     delete file_ptr;
-    ++gPerfCounters->m_ROFileUnmap;
+    __sync_add_and_fetch(&gPerfCounters->m_ROFileUnmap, 1);
 
     return;
 
@@ -837,7 +835,7 @@ void BGFileUnmapper2(void * arg)
 #endif
 
     delete file_ptr;
-    ++gPerfCounters->m_RWFileUnmap;
+    __sync_add_and_fetch(&gPerfCounters->m_RWFileUnmap, 1);
 
     return;
 
@@ -847,7 +845,7 @@ void BGFileUnmapper2(void * arg)
 
 // how many blocks of 3 priority background threads/queues
 /// for riak, make sure this is an odd number (and especially not 4)
-#define THREAD_BLOCKS 7
+#define THREAD_BLOCKS 3
 
 static pthread_once_t once = PTHREAD_ONCE_INIT;
 static Env* default_env[THREAD_BLOCKS];
@@ -879,8 +877,6 @@ static void InitDefaultEnv()
                       fd, 0);
         }   // if
 
-        syslog(LOG_WARNING, "mapped to %p", base);
-
         if (MAP_FAILED != base)
         {
             PerformanceCounters * perf;
@@ -889,16 +885,13 @@ static void InitDefaultEnv()
             if (!perf->VersionTest())
                 perf->Init();
 
-
-            syslog(LOG_WARNING, "before %p", &gPerfCounters->m_WriteError);
             gPerfCounters=perf;
-            syslog(LOG_WARNING, "after %p", &gPerfCounters->m_WriteError);
         }   // if
 
     }   // if
     else
     {
-        syslog(LOG_WARNING, "file failed to open %d", errno);
+//        syslog(LOG_WARNING, "file failed to open %d", errno);
     }   // else
 }
 
