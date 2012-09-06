7a80c10008b194786a15e63e2a6b5b37b85d2be4
Matthew V <matthewv@basho.com>
2012-09-06 17:41:26 -0400

    Adjust the polling timeout from 5 to 1 per customer feedback.

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -1079,7 +1079,7 @@ DBImpl::PrioritizeWork(
         // pause to potentially hand off disk to
         //  memtable threads
         clock_gettime(CLOCK_REALTIME, &timeout);
-        timeout.tv_sec+=5;
+        timeout.tv_sec+=1;
         ret_val=pthread_rwlock_timedwrlock(&gThreadLock0, &timeout);
         if (0==ret_val)
             pthread_rwlock_unlock(&gThreadLock0);
@@ -1087,10 +1087,10 @@ DBImpl::PrioritizeWork(
 
         // Give priorities to level 0 compactions, unless
         //  this compaction is blocking a level 0 in this database
-        if (!IsLevel0 && level0_good)
+        if (!IsLevel0 && level0_good && !again)
         {
             clock_gettime(CLOCK_REALTIME, &timeout);
-            timeout.tv_sec+=5;
+            timeout.tv_sec+=1;
             ret_val=pthread_rwlock_timedwrlock(&gThreadLock1, &timeout);
             if (0==ret_val)
                 pthread_rwlock_unlock(&gThreadLock1);
