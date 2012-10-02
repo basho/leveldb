4ddab8216e23644807c789ca73e257026bda675b
mmaszewski <mmaszewski@basho0.matthewv.org>
2012-10-02 12:57:46 -0400

    Corrections to the temporary key storage and usage.  Still think there is a bug.

--- util/crc32c.cc
+++ util/crc32c.cc
@@ -283,11 +283,18 @@ static inline uint32_t LE_LOAD32(const uint8_t *p) {
   return DecodeFixed32(reinterpret_cast<const char*>(p));
 }
 
+
+uint32_t
+Crc32c(
+    uint32_t StartCrc,
+    void * BlockStart,
+    size_t BlockSize);
+
 uint32_t Extend(uint32_t crc, const char* buf, size_t size) {
   const uint8_t *p = reinterpret_cast<const uint8_t *>(buf);
   const uint8_t *e = p + size;
   uint32_t l = crc ^ 0xffffffffu;
-
+#if 0
 #define STEP1 do {                              \
     int c = (l & 0xff) ^ *p++;                  \
     l = table0_[c] ^ (l >> 8);                  \
@@ -325,8 +332,56 @@ uint32_t Extend(uint32_t crc, const char* buf, size_t size) {
   }
 #undef STEP4
 #undef STEP1
+
+#else
+  l=Crc32c(crc ^ 0xffffffffu, (void*)buf, size);
+#endif
+
   return l ^ 0xffffffffu;
 }
+
+
+
+uint32_t
+Crc32c(
+    uint32_t StartCrc,
+    void * BlockStart,
+    size_t BlockSize)
+{
+    size_t fullqwords, remainder;
+    uint32_t ret_crc;
+    char * src_c;
+    uint64_t * src_q;
+
+    fullqwords=BlockSize / 8;
+    remainder=BlockSize % 8;
+
+//    remainder=BlockSize;
+
+    ret_crc=StartCrc;
+    src_q=(uint64_t *)BlockStart;
+#if 1
+    for ( ; 0!=fullqwords; --fullqwords, ++src_q)
+    {
+        __asm__ __volatile__ (
+            ".byte 0xf2, 0x48, 0x0f, 0x38, 0xf1, 0xf1;"
+            : "=S"(ret_crc)
+            : "S"(ret_crc), "c"(*src_q));
+    }   // for
+#endif
+    src_c=(char *)src_q;
+    for ( ; 0!=remainder; --remainder, ++src_c)
+    {
+        __asm__ __volatile__ (
+            ".byte 0xf2, 0x48, 0x0f, 0x38, 0xf0, 0xf1;"
+            : "=S"(ret_crc)
+            : "S"(ret_crc), "c"(*src_c));
+    }   // for
+
+    return(ret_crc);
+
+}   // Crc32c
+
 
 }  // namespace crc32c
 }  // namespace leveldb
