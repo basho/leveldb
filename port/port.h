ef52789c23a1e160d2aa68883c59bcf2556cc4a3
MatthewVon <matthewv@basho.com>
2015-12-31 15:11:27 -0500

    Create ldb_config.h that holds compile time defines for external users of leveldb internal code

--- port/port.h
+++ port/port.h
@@ -6,6 +6,7 @@
 #define STORAGE_LEVELDB_PORT_PORT_H_
 
 #include <string.h>
+#include "leveldb/ldb_config.h"
 
 // Include the appropriate platform specific file below.  If you are
 // porting to a new platform, see "port_example.h" for documentation
