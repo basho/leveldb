e5d0ea5de90a5799232f841e0d1ef2dd40fe1cf6
Dan Horák <dan@danny.cz>
2016-06-03 04:48:02 -0400

    Switch from <cstdatomic> to <atomic>
    Switch from <cstdatomic> to <atomic>. The former never made it into the
    standard and doesn't exist in modern gcc versions at all.  The later contains
    everything that leveldb was using from the former.
    
    Backported from Google leveldb 1.18
    https://github.com/google/leveldb/commit/803d69203a62faf50f1b77897310a3a1fcae712b

--- port/atomic_pointer.h
+++ port/atomic_pointer.h
@@ -20,9 +20,9 @@
 #define PORT_ATOMIC_POINTER_H_
 
 #include <stdint.h>
-//#ifdef LEVELDB_CSTDATOMIC_PRESENT
-//#include <cstdatomic>              ... moved below
-//#endif
+#ifdef LEVELDB_ATOMIC_PRESENT
+#include <atomic>
+#endif
 #ifdef OS_WIN
 #include <windows.h>
 #endif
@@ -114,9 +114,8 @@ class AtomicPointer {
   }
 };
 
-// AtomicPointer based on <cstdatomic>
-#elif defined(LEVELDB_CSTDATOMIC_PRESENT)
-#include <cstdatomic>
+// AtomicPointer based on <atomic>
+#elif defined(LEVELDB_ATOMIC_PRESENT)
 
 class AtomicPointer {
  private:
