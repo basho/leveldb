9da58cc7e61eac18da5a099fd249c89cd7e0636a
Matthew V <matthewv@basho.com>
2016-09-05 15:31:40 -0400

    correct copyright years.

--- include/leveldb/perf_count.h
+++ include/leveldb/perf_count.h
@@ -2,7 +2,7 @@
 //
 // perf_count.h:  performance counters LevelDB
 //
-// Copyright (c) 2012-2015 Basho Technologies, Inc. All Rights Reserved.
+// Copyright (c) 2012-2016 Basho Technologies, Inc. All Rights Reserved.
 //
 // This file is provided to you under the Apache License,
 // Version 2.0 (the "License"); you may not use this file
--- util/hot_backup.h
+++ util/hot_backup.h
@@ -2,7 +2,7 @@
 //
 // hot_backup.h
 //
-// Copyright (c) 2011-2016 Basho Technologies, Inc. All Rights Reserved.
+// Copyright (c) 2016 Basho Technologies, Inc. All Rights Reserved.
 //
 // This file is provided to you under the Apache License,
 // Version 2.0 (the "License"); you may not use this file
